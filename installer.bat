b4a4a06553c8d7d8cd2e7f165a9d69d0b840e882
johnman3032 <134149718+johnman3032@users.noreply.github.com>
2023-10-19 13:54:18 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -9,3 +9,4 @@ mkdir instance-imgs-sdxl
 mkdir output-path
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
+pip install bitsandbytes_windows

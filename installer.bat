3d66ddae89326e241993d3f5a2730f79aa452700
johnman3032 <134149718+johnman3032@users.noreply.github.com>
2023-10-19 12:13:02 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,11 @@
 @echo on
 call python -m venv venv
 call venv\scripts\activate
+mkdir base-model-path
+mkdir base-model-path-sdxl
+mkdir class-imgs-db
+mkdir instance-imgs
+mkdir instance-imgs-sdxl
+mkdir output-path
 pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
